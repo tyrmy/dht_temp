6b26e42bf6b02ef68adabe19c5d280203c40401d
Lassi Lehtinen <lokleh@utu.fi>
2020-02-26 15:39:26 +0000

    GUI Class implemented

--- dht_temp_gui.py
+++ dht_temp_gui.py
@@ -1,42 +1,33 @@
-from os import system
 from tkinter import *
-
 import Adafruit_DHT
-import time, threading, schedule
 
-DHT_SENSOR = Adafruit_DHT.DHT22
-DHT_PIN = 4
+class tempApp():
+    def __init__(self):
+        self.root = Tk()
+        self.temperatureLabel = Label(text="TEM")
+        self.humidityLabel = Label(text="HUM")
+        self.hum = Label(text="Humidity:")
+        self.tem = Label(text="Temperature:")
 
-def getTempAndHumidity():
-    humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
-    if humidity is not None and temperature is not None:
-        return humidity, temperature
-    else:
-        return None
+        self.temperatureLabel.grid(row=0, column=1)
+        self.humidityLabel.grid(row=1,column=1)
+        self.tem.grid(row=0, column=0)
+        self.hum.grid(row=1,column=0)
 
-root = Tk()
+        self.updateValues()
+        self.root.mainloop()
 
-readings = tuple()
-temp = StringVar()
-hum = StringVar()
-temp.set("TEMP")
-hum.set("HUM")
+    def updateValues(self):
+        DHT_SENSOR = Adafruit_DHT.DHT22
+        DHT_PIN = 4
 
-def updateReadings():
-#    while True:
-        readings = getTempAndHumidity()
-        hum.set("{0:0.1f}%".format(readings[0]))
-        temp.set("{0:0.1f}*C".format(readings[1]))
-        print("Updated!")
- #       time.sleep(5)
+        humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
+        if humidity is not None and temperature is not None:
+            self.humidityLabel.configure(text="{0:0.1f}%".format(humidity))
+            self.temperatureLabel.configure(text="{0:0.1f}*C".format(temperature))
+        else:
+            self.humidityLabel.configure(text="...")
+            self.temperatureLabel.configure(text="...")
+        self.root.after(3000, self.updateValues)
 
-Label(root,textvariable=temp).grid(row=0, column=1)
-Label(root,textvariable=hum).grid(row=1, column=1)
-Label(root, text="Tempterature:").grid(row=0, column=0)
-Label(root, text="Humidity:").grid(row=1, column=0)
-
-updateReadings()
-#schedule.every(5).seconds.do(updateReadings)
-#threading.Thread(target=updateReadings, daemon=True).start()
-
-mainloop()
+app = tempApp()
