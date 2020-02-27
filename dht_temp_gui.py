1f1c9bb70c3e31bbcff719687ed8952eaf7b25e9
Lassi Lehtinen <lokleh@utu.fi>
2020-02-27 14:43:00 +0000

    Better GUI

--- dht_temp_gui.py
+++ dht_temp_gui.py
@@ -1,26 +1,35 @@
 from tkinter import *
 import Adafruit_DHT
 
+DHT_SENSOR = Adafruit_DHT.DHT22
+DHT_PIN = 4
+
 class tempApp():
     def __init__(self):
         self.root = Tk()
+        self.root.title("DHT22 Temperature App")
         self.temperatureLabel = Label(text="TEM")
         self.humidityLabel = Label(text="HUM")
         self.hum = Label(text="Humidity:")
         self.tem = Label(text="Temperature:")
 
-        self.temperatureLabel.grid(row=0, column=1)
-        self.humidityLabel.grid(row=1,column=1)
-        self.tem.grid(row=0, column=0)
-        self.hum.grid(row=1,column=0)
+        self.temperatureLabel.grid(row=0, column=2)
+        self.humidityLabel.grid(row=1,column=2)
+        self.tem.grid(row=0, column=1)
+        self.hum.grid(row=1,column=1)
+
+        self.root.grid_rowconfigure(0, weight=1)
+        self.root.grid_rowconfigure(3, weight=1)
+        self.root.grid_columnconfigure(0, weight=1)
+        self.root.grid_columnconfigure(3, weight=1)
 
         self.updateValues()
+        #self.root.grid(sticky="nsew")
+        self.root.minsize(300,100)
+
         self.root.mainloop()
 
     def updateValues(self):
-        DHT_SENSOR = Adafruit_DHT.DHT22
-        DHT_PIN = 4
-
         humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
         if humidity is not None and temperature is not None:
             self.humidityLabel.configure(text="{0:0.1f}%".format(humidity))
@@ -28,6 +37,6 @@ class tempApp():
         else:
             self.humidityLabel.configure(text="...")
             self.temperatureLabel.configure(text="...")
-        self.root.after(3000, self.updateValues)
+        self.root.after(5000, self.updateValues)
 
 app = tempApp()
