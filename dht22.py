24133d702e34d47e8fa3bf67e02953ac1172ca47
Lassi Lehtinen <lokleh@utu.fi>
2020-05-12 01:54:34 +0300

    lcd printing method added

--- dht22.py
+++ dht22.py
@@ -1,6 +1,5 @@
-from os import system
-
 import Adafruit_DHT
+from Adafruit_CharLCD import Adafruit_CharLCD
 from time import sleep
 
 DHT_SENSOR = Adafruit_DHT.DHT22
@@ -11,7 +10,6 @@ def continuous_print():
         while True:
             humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
             if humidity is not None and temperature is not None:
-                #system('clear')
                 print("T={0:5.1f}*C H={1:5.1f}%".format(temperature, humidity))
             else:
                 print("Failed to retrieve data from sensor...")
@@ -27,5 +25,18 @@ def get_values():
         print("get_values: fetch error")
         return None
 
+def print_to_lcd():
+    try:
+        lcd = Adafruit_CharLCD(rs=26, en=19, d4=13, d5=6, d6=5, d7=21, cols=16, lines=2)
+        hum, temp = get_values()
+        lcd.set_cursor(0,0)
+        lcd.message("Temp: %d C" % temp)
+        lcd.set_cursor(0,1)
+        lcd.message("Humi: %d %" % temp)
+        sleep(3)
+    except KeyboardInterrupt:
+        print('Exiting...')
+
 if __name__ == "__main__":
-    continuous_print()
+    #continuous_print()
+    print_to_lcd()
